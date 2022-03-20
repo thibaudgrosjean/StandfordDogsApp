870e2b6ab67e23b372123998b1cdeae6c7a813f4
Grosjean Thibaud <grosjeanthibaud@Grosjeans-MacBook-Air.local>
2022-03-20 22:30:22 +0100

    drag and drop

---
+++ streamlit_app.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+import time
+
+'Dogs race predictor'
+
+uploaded_file = st.file_uploader('Choose picture file')
+
+if uploaded_file is not None:
+
+    with st.spinner('Predicting...'):
+
+        st.image(uploaded_file)
+
+        time.sleep(5)
+        
+        try: st.success('Caniche, probabilité: 56.08%')
+        except: st.error('Error, please contact the assistance')
+    # file =..
+    # pred method
